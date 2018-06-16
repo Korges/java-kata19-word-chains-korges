5762783bfd741f3090b2dc34fe7686a8768fd799
Korges <korgesik@gmail.com>
2018-06-16 23:01:33 +0200

    Add SampleRunTest with 2 test cases. One with existing connection between graphs nodes, second without.

---
+++ src/test/java/com/korges/krk/SampleRunTest.java
@@ -0,0 +1,53 @@
+package com.korges.krk;
+
+import com.korges.krk.exception.ConnectionNotFoundException;
+import com.korges.krk.exception.WordNotInWordListException;
+import com.korges.krk.graph.Word;
+import com.korges.krk.graph.WordGraph;
+import com.korges.krk.program.body.ChainFinder;
+import org.junit.jupiter.api.Test;
+import java.util.LinkedList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SampleRunTest {
+
+    private static WordGraph wordGraph;
+    private static ChainFinder chainFinder;
+    private static String[] sampleInputs;
+
+    @Test
+    void findShortestPathTestIfConnectionExist() throws WordNotInWordListException, ConnectionNotFoundException {
+
+        sampleInputs = new String[]{"dog", "cat"};
+        chainFinder = new ChainFinder(sampleInputs[0].length());
+        chainFinder.buildConnectionsAndGetEndPoints(sampleInputs[0], sampleInputs[1]);
+        wordGraph = chainFinder.getWordGraph();
+
+        Word sourceTest = wordGraph.getWordByValue("dog");
+        Word destinationTest = wordGraph.getWordByValue("cat");
+
+        LinkedList<Word> testList = wordGraph.breadthFirstSearch(sourceTest, destinationTest);
+
+        LinkedList<Word> expectedList = new LinkedList<>();
+        expectedList.add(wordGraph.getWordByValue("dog"));
+        expectedList.add(wordGraph.getWordByValue("dot"));
+        expectedList.add(wordGraph.getWordByValue("dat"));
+        expectedList.add(wordGraph.getWordByValue("cat"));
+
+        assertEquals(expectedList.toString(), testList.toString());
+    }
+
+    @Test
+    void findShortestPathTestIfConnectionNotExist() throws WordNotInWordListException {
+        sampleInputs = new String[]{"benson", "benoni"};
+        chainFinder = new ChainFinder(sampleInputs[0].length());
+        wordGraph = chainFinder.getWordGraph();
+
+        Word sourceTest = wordGraph.getWordByValue("benson");
+        Word destinationTest = wordGraph.getWordByValue("benoni");
+
+        assertThrows(ConnectionNotFoundException.class, () -> wordGraph.breadthFirstSearch(sourceTest, destinationTest));
+
+    }
+}
