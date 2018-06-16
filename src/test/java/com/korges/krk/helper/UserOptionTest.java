7fb8ba3493afa76adfda1c9643b39386ce9787a0
Korges <korgesik@gmail.com>
2018-06-16 22:51:28 +0200

    Add UserOptionTest

---
+++ src/test/java/com/korges/krk/helper/UserOptionTest.java
@@ -0,0 +1,21 @@
+package com.korges.krk.helper;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserOptionTest {
+
+    private static UserOption userOption;
+
+    @BeforeAll
+    static void createUserOption() {
+        userOption = UserOption.START;
+    }
+    @Test
+    void recognizeUserOption() {
+
+        assertEquals(UserOption.EXIT, userOption.recognizeUserOption("EXIT"));
+    }
+}
